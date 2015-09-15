c3c6d51cf4c4251bb4ad07c6e59841be17e0c5eb
HeyStenson <h.n.stenson@gmail.com>
2015-09-15 16:17:18 -0700

    format_name passing test

--- username.rb
+++ username.rb
@@ -2,7 +2,17 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+  if first == "" || last == ""
+  	nil
+  else 
+  	first.gsub!(/[^A-Za-z]/, '')
+  	last.gsub!(/[^A-Za-z]/, '')
+  	first = first.strip
+	last = last.split.join("").strip
+	user_name = first[0] + last
+	user_name = user_name.downcase 
+  end
+  user_name
 end
 
 def format_year
