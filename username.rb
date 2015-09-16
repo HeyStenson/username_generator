3f4d757d8efcefa74ecc22dbae567a233d8ae343
HeyStenson <h.n.stenson@gmail.com>
2015-09-15 18:01:37 -0700

    more pair programming

--- username.rb
+++ username.rb
@@ -15,10 +15,41 @@ def format_name(first, last)
   user_name
 end
 
-def format_year
-  nil
+def format_year(year)
+	if year.to_s.length != 4
+		nil
+	else 
+	year = year.to_s
+	year[2..3] 
+  	end
 end
 
-def build_username
-  nil
+def build_username(first, last, year, level = 0)
+	base_username = format_name(first, last)
+	user_year = format_year(year)
+	username = base_username.strip + user_year
+	if level === 1
+		username = "seller-" + username
+	elsif level === 2
+		username = "manager-" + username
+	elsif level === 3
+		username = "admin-" + username
+	else
+		username
+	end
+end
+
+
+$all_usernames = Hash.new
+def generate_username(first, last, year, level = 0)
+	new_user = build_username(first, last, year, level = 0)
+	if !$all_usernames.has_key? new_user
+		$all_usernames[new_user] = 0
+		new_user
+	else
+		$all_usernames[new_user] = $all_usernames[new_user] + 1
+		new_user = new_user + "_" + $all_usernames[new_user].to_s
+		new_user
+	end
+
 end
